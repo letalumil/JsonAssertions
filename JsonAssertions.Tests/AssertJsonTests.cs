5234a740d88017be9da9ddde641827bee8731844
letalumil <letalumil@gmail.com>
2014-10-28 23:16:52 +0300

    Add number of properties comparison

--- JsonAssertions.Tests/AssertJsonTests.cs
+++ JsonAssertions.Tests/AssertJsonTests.cs
@@ -6,9 +6,48 @@ namespace JsonAssertions.Tests
     public class AssertJsonTests
     {
         [Test]
-        public void AreEquals_EqualJsonStrings_Success()
+        public void EqualJsonStrings_Success()
         {
             AssertJson.AreEquals("{name:'value'}", "{name:'value'}");
+        }
+
+        [Test]
+        public void AreEquals_UnequalPropertyValues_Fail()
+        {
+            const string expectedObject = "{name:'value'}";
+            const string actualObject = "{name:'value2'}";
+
+            var assertionException =
+                Assert.Throws<AssertionException>(() => AssertJson.AreEquals(expectedObject, actualObject));
+
+            Assert.AreEqual(assertionException.Message,
+                string.Format("Property \"{0}\" does not match\r\n" +
+                              "Expected: {1}\r\n" +
+                              "But was: {2}", "name", "value", "value2"));
+        }
+
+        [Test]
+        public void AreEquals_MissingProperty_Fail()
+        {
+            const string expectedObject = "{name:'value', missing:'yes'}";
+            const string actualObject = "{name:'value'}";
+            var assertionException =
+                Assert.Throws<AssertionException>(() => AssertJson.AreEquals(expectedObject, actualObject));
+
+            Assert.AreEqual(assertionException.Message,
+                string.Format("Object  properties missing: missing"));
+        }
+
+        [Test]
+        public void AreEquals_ExcessProperty_Fail()
+        {
+            const string expectedObject = "{name:'value'}";
+            const string actualObject = "{name:'value', excess:'yes'}";
+            var assertionException =
+                Assert.Throws<AssertionException>(() => AssertJson.AreEquals(expectedObject, actualObject));
+
+            Assert.AreEqual(assertionException.Message,
+                string.Format("Object  has excess properties: excess"));
         }
     }
 }
--- JsonAssertions/AssertJson.cs
+++ JsonAssertions/AssertJson.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 
 namespace JsonAssertions
 {
@@ -15,6 +17,34 @@ namespace JsonAssertions
         {
             if (JToken.DeepEquals(expectedJObject, actualJObject))
                 return;
+
+            EqualNumberOfProperties(expectedJObject, actualJObject);
+
+            foreach (var property in expectedJObject)
+            {
+                if (actualJObject[property.Key] != expectedJObject[property.Key])
+                    Assert.Fail("Property \"{0}\" does not match\r\n" +
+                                "Expected: {1}\r\n" +
+                                "But was: {2}", property.Key, property.Value, actualJObject[property.Key]);
+            }
+        }
+
+        public static void EqualNumberOfProperties(JObject expectedJObject, JObject actualJObject)
+        {
+            var expectedProperties = expectedJObject.Children<JProperty>().Select(property => property.Name).ToList();
+            var actualProperties = actualJObject.Children<JProperty>().Select(property => property.Name).ToList();
+
+            var missingProperties = expectedProperties.Except(actualProperties).ToList();
+            var excessProperties = actualProperties.Except(expectedProperties).ToList();
+
+            if (missingProperties.Any())
+                Assert.Fail("Object {0} properties missing: {1}",
+                    expectedJObject.Path,
+                    string.Join(", ", missingProperties));
+
+            if (excessProperties.Any())
+                Assert.Fail("Object {0} has excess properties: {1}", expectedJObject.Path,
+                    string.Join(", ", excessProperties));
         }
     }
 }
