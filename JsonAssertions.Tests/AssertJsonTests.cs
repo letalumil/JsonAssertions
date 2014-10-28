99baa2ae707c6eba7a5a744849779daf78f18c85
letalumil <letalumil@gmail.com>
2014-10-28 22:23:43 +0300

    Initial commit

---
+++ JsonAssertions.Tests/AssertJsonTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace JsonAssertions.Tests
+{
+    [TestFixture]
+    public class AssertJsonTests
+    {
+        [Test]
+        public void AreEquals_EqualJsonStrings_Success()
+        {
+            AssertJson.AreEquals("{name:'value'}", "{name:'value'}");
+        }
+    }
+}
---
+++ JsonAssertions/AssertJson.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace JsonAssertions
+{
+    public static class AssertJson
+    {
+        public static void AreEquals(string expectedJson, string actualJson)
+        {
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+            AreEquals(expectedJObject, actualJObject);
+        }
+
+        public static void AreEquals(JObject expectedJObject, JObject actualJObject)
+        {
+            if (JToken.DeepEquals(expectedJObject, actualJObject))
+                return;
+        }
+    }
+}
